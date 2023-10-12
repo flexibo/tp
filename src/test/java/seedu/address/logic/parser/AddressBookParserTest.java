5e58768fbb0aedbf10b617faea87c66adfe8e83f
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-10-13 01:30:42 +0800

    Fix Checkstyles

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -13,14 +13,14 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.contacts.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.contacts.AddCommand;
 import seedu.address.logic.commands.contacts.DeleteCommand;
 import seedu.address.logic.commands.contacts.EditCommand;
 import seedu.address.logic.commands.contacts.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.contacts.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.contacts.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
