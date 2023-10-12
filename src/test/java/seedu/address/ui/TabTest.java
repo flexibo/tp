aceb7e9f6bcd4e89e20c4c3feeb118ed99ff3375
Debbie Hii Wenxin <e0959749@u.nus.edu>
2023-10-13 01:52:39 +0800

    Add Tab Test

---
+++ src/test/java/seedu/address/ui/TabTest.java
@@ -0,0 +1,13 @@
+package seedu.address.ui;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.ui.tab.EventsTab;
+
+
+public class TabTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assert(new EventsTab().equals(new EventsTab()));
+    }
+
+}
